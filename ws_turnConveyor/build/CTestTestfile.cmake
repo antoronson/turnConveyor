e3c9a91c03893a78d393b730e7b97da3e6a95072
antoronson <antoronson590@gmail.com>
2021-10-02 19:46:48 +0200

    BAse Data

---
+++ ws_turnConveyor/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/antoronson/git_trade/turnConveyor/ws_turnConveyor/src
+# Build directory: /home/antoronson/git_trade/turnConveyor/ws_turnConveyor/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ ws_turnConveyor/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/antoronson/git_trade/turnConveyor/ws_turnConveyor/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
