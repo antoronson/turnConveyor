00927405535b3cf4e15d3e9ae9781c02a276d9f1
antoronson <antoronson590@gmail.com>
2021-10-10 22:05:54 +0200

    Ros Working FIles

--- ws_turnConveyor/src/user_pkg/scripts/calcVelocity.py
+++ ws_turnConveyor/src/user_pkg/scripts/calcVelocity.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 
 import rospy
+import message_filters
 from std_msgs.msg import *
 
-def bGetTurnConvReady(msg):
-   # print(msg.data)
-    bHandover = msg.data
-    #print(bHandover)
+def callback0(msg):
+    #global bStartturnConv
+    bStartturnConv = msg.data
 
-def bStartTurnProcess(msg):
-    self.bStartToTurn = msg.data
+def callback1(msg):
+    bStartTurn = msg.data
 
-def bStopTurnProcess(msg):
+def callback2(msg):
     bStopTurn = msg.data
-
 
 def VelocityCalculation():
     rospy.init_node('convSpeed', anonymous=True)
@@ -21,16 +20,25 @@ def VelocityCalculation():
 
     pubVelocity = rospy.Publisher('dConvSpeed', Float64, queue_size=10)
     #while not rospy.is_shutdown():
-    rospy.Subscriber("startTurnConveyor",Bool, bGetTurnConvReady)
-    rospy.Subscriber("startTurnProcess", Bool, bStartTurnProcess)
-    rospy.Subscriber("stopTurnProcess", Bool, bStopTurnProcess)
+    rospy.Subscriber("startTurnConveyor",Bool, callback0)
+    rospy.Subscriber("startTurnProcess", Bool, callback1)
+    rospy.Subscriber("stopTurnProcess", Bool, callback2)
+    #ts = message_filters.TimeSynchronizer([bHandover, bStartTurn, bStopTurn], 3)
+
+    #ts.registerCallback(callback)
+
     #print(bGetTurnConvReady.bHandover)
-    rospy.spin()
-    #while not rospy.is_shutdown():
-    #    print(bGetConvReady.bHandover, "\t", bStartToTurn, "\t", bStopTurn)
+    #rospy.spin()
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        print(bStartturnConv, "\t", bStartTurn, "\t", bStopTurn)
+        rate.sleep()
+        rospy.spin()
     #   print()
 
 
 if __name__ == '__main__':
-
+    bStartturnConv = False
+    bStartTurn = False
+    bStopTurn = False
     VelocityCalculation()
